59ea7671e77177d81be80e4023c62f12e75e3345
Gautam Singh Dhakrey <gautamdhakrey11@gmail.com>
2026-06-17 11:32:32 +0530

    add scenario based

---
+++ core-java-practice/ScenarioBased/MovieReservation.java
@@ -0,0 +1,20 @@
+public class MovieReservation {
+
+    static int seats[] = {101, 102, 103, 104, 105};
+
+    public static int getSeat(int index) {
+
+        try {
+            return seats[index];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid seat position.");
+            return -1;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(getSeat(2));
+        System.out.println(getSeat(8));
+    }
+}
